32fdd60c19cad63a4e00812641053746fe1f7fce
LeonardoCampello-dev <leonardocampello.dev@gmail.com>
2021-04-07 00:08:57 -0300

    ✨ New Feature: Create Global Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { GlobalStyle } from "./styles/global";
+
 export function App() {
   return (
     <div className="App">
-      <h1>Opa</h1>
+      <GlobalStyle />
+      <h1>Opa meu amigo</h1>
     </div>
   );
 }
