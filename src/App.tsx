9c497726fbb4d506dbb3ef25b897296280f43c9e
LeonardoCampello-dev <leonardocampello.dev@gmail.com>
2021-04-07 23:28:48 -0300

    💥 NEW: Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { Fragment } from "react";
+
+import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <div className="App">
+    <Fragment>
+      <Header />
       <GlobalStyle />
-      <h1>Opa meu amigo</h1>
-    </div>
+    </Fragment>
   );
 }
