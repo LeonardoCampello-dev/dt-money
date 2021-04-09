33f107959623e8187d6f559297a39d2f3de13b6a
LeonardoCampello-dev <leonardocampello.dev@gmail.com>
2021-04-08 23:15:52 -0300

    💥 NEW: Component: Summary.tsx and TransactionsTable.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Fragment } from "react";
 
 import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
+
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <Fragment>
       <Header />
+      <Dashboard />
       <GlobalStyle />
     </Fragment>
   );
