95e966683fa9e62410c1074cbd911fd05c3049c9
Leonardo Campello <leonardocampello.dev@gmail.com>
2021-04-10 23:01:19 -0300

    🚧 Work in Progress: Creating Modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 import { Header } from "./components/Header";
 import { Dashboard } from "./components/Dashboard";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(
+    false
+  );
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <Fragment>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </Fragment>
   );
